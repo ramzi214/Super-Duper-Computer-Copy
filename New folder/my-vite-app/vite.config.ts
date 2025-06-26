6de5ce6092120fe0cb37b9206c6dc9256a57b7d7
ramzi214 <44089032+ramzi214@users.noreply.github.com>
2025-06-26 01:54:07 -0500

    Remove project files for my Vite application

--- New folder/my-vite-app/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/my-vite-app/', // Set the base path for GitHub Pages
-  build: {
-    outDir: 'dist', // Output directory for the build
-  },
-});
